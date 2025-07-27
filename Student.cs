85d84382e73a41bc83dfc1f4fb70b2815f4f0eeb
SiphepheloMyeni <217014127@stu.ukzn.ac.za>
2025-07-27 15:06:09 +0200

    Created Student Class

---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Record_Management
+{
+    internal class Student
+    {
+        // Properties to store student ID, name, and mark
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Mark { get; set; }
+
+        // Constructor to initialize a student object
+        public Student(int id, string name, double mark)
+        {
+            ID = id;
+            Name = name;
+            Mark = mark;
+        }
+
+        // ToString() for easy display in ListBox
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Mark: {Mark}";
+        }
+    }
+}
